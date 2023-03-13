b836a2ed2c1fedadbb7ac5f69d41c62b6cbd3022
ahmetenesbahar <enesbahar2303@gmail.com>
2023-03-13 15:56:42 +0300

    go prev/next/random handles added

--- src/Review.jsx
+++ src/Review.jsx
@@ -30,6 +30,13 @@ function Reviews() {
     });
   };
 
+  const goRandom = () => {
+    setIndex(() => {
+      let newIndex = Math.floor(Math.random() * data.length);
+      return checkNumber(newIndex);
+    });
+  };
+
   return (
     <>
       <img src={image} alt={name} />
@@ -39,6 +46,27 @@ function Reviews() {
       </div>
       <div className="text-box">
         <p>{text}</p>
+      </div>
+      <div className="arrows">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="48"
+          viewBox="0 96 960 960"
+          width="48"
+          onClick={goPrev}
+        >
+          <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+        </svg>
+        <button onClick={goRandom}> Random Comment !</button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="48"
+          viewBox="0 96 960 960"
+          width="48"
+          onClick={goNext}
+        >
+          <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+        </svg>
       </div>
     </>
   );
